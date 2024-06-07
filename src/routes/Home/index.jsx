3e2cae1d08c6a2bee36004dba4a8c0783deecbf2
Andres Oliveira <andresoliveira@protonmail.com>
2024-06-07 10:55:01 -0300

    Home structure
    - Home container structure

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+
 export default function Home() {
   return (
-    <h1>home</h1>
+    <main>
+      <div className='home-container'>
+        <img src={logo} className='w-full md:w-[600px]'/>
+        <h1>Bem vindo ao <strong>Kompass</strong></h1>
+        <h1>Encontre quem você procura</h1>
+        <NavLink to="/location">
+          <button className='button'>Começar</button>
+        </NavLink>
+      </div>
+    </main>
   )
 }
