f253ade1a9fd6e7e172499cb14590f9644cf6890
Andres Oliveira <andresoliveira@protonmail.com>
2024-06-11 01:58:55 -0300

    Added marker functions
    - Added marked passed by props (element) on Marker
    - Added handle click show/hide info window
    - Added Info Window content

--- src/components/GoogleMaps/index.jsx
+++ src/components/GoogleMaps/index.jsx
@@ -1,9 +1,14 @@
-import { APIProvider, InfoWindow, Map, Marker, useMarkerRef } from "@vis.gl/react-google-maps"
+import { APIProvider, InfoWindow, Map, Marker } from "@vis.gl/react-google-maps"
+import { useState } from "react"
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
 export default function GoogleMaps({ data }) {
   const users = data
-  const [markerRef, marker] = useMarkerRef();
+  // const [markerRef, marker] = useMarkerRef();
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [activeMarker, setActiveMarker] = useState(null);
+  const [showInfoWindow, setShowInfoWindow] = useState(true);
 
   return (
     <div className="w-full flex flex-col items-center justify-center">
@@ -19,17 +24,31 @@ export default function GoogleMaps({ data }) {
           {users.map((user) => (
             <>
               <Marker
-                ref={markerRef}
                 key={user.id}
                 position={{lat: Number(user.address.geo.lat), lng: Number(user.address.geo.lng)}}
+                onClick={(marker) => {
+                  setSelectedElement(user);
+                  setActiveMarker(marker);
+                }}
               />
-              <InfoWindow
-                anchor={marker}
-              >
-                <p>{user.name}</p>
-              </InfoWindow>
             </>
           ))}
+          {selectedElement ? (
+            <InfoWindow
+              position={{lat: Number(selectedElement.address.geo.lat), lng: Number(selectedElement.address.geo.lng)}}
+              visible={showInfoWindow}
+              marker={activeMarker}
+              onCloseClick={() => {
+                setSelectedElement(null);
+              }}
+            >
+              <div className="flex flex-col items-center justify-center bg-slate-900 w-full rounded-lg h-40 text-white p-4">
+                <h2>{selectedElement.name}</h2>
+                <span>City: {selectedElement.address.city}</span>
+                <small>lat: {selectedElement.address.geo.lat} / lng: {selectedElement.address.geo.lng}</small>
+              </div>
+            </InfoWindow>
+          ) : null}
         </Map>
       </APIProvider>
     </div>
