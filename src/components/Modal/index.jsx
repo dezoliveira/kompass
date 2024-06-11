a090ab9a8a27e412060ea4c58eef19371424709e
Andrés Oliveira <85899665+dezoliveira@users.noreply.github.com>
2024-06-11 22:37:18 +0000

    refacotor modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -10,12 +10,18 @@ export default function Modal({ children, setShowModal }) {
 
   return (
     <>
-      <div className="backdrop absolute top-0 left-0 w-full h-full z-10 bg-slate-950 bg-opacity-55 flex flex-col justify-center items-center">
-        <div className="modal relative w-[80vh] h-[80vh] z-20 bg-slate-50 flex flex-col align-center justify-center">
-          <span className="absolute right-2 top-2 z-20" onClick={(e) => handleModal(e)}>
-            <FontAwesomeIcon icon={faClose} fontSize={32} className='text-slate-50 hover:cursor-pointer hover:text-fuchsia-400'/>
+      <div className="backdrop">
+        <div className="modal">
+          <span
+            className="close-button"
+            onClick={(e) => handleModal(e)}
+          >
+            <FontAwesomeIcon
+              icon={faClose}
+              fontSize={32}
+              className='close-icon'/>
           </span>
-          <div className="modal-title w-full bg-slate-900 text-slate-50 absolute top-0 p-4 flex items-center justify-center w-full">
+          <div className="modal-title p-4">
             <h1>Modal Title here</h1>
           </div>
           <div>
